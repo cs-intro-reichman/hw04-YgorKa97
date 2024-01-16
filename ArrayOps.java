819cf9bf8899db6b5dd4ca5579df0890f7957db4
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2024-01-16 11:06:17 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,77 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+
     }
 
-    public static int secondMaxValue(int [] array) {
+    public static int findMissingInt(int[] array) {
         // Write your code here:
-        return 0;
+        int sumMiss = 0;
+        int sumFull = 0;
+        int missNum = 0;
+        int[] fullArr = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            sumMiss += array[i];
+        }
+        for (int j = 0; j < fullArr.length; j++) {
+            fullArr[j] = j;
+            sumFull += fullArr[j];
+        }
+        missNum = sumFull - sumMiss;
+        return missNum;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static int secondMaxValue(int[] array) {
         // Write your code here:
-        return false;
+        int max = array[0];
+        int secondMax = array[0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+                if (array[j] > secondMax && array[j] < max) {
+                    secondMax = array[j];
+                }
+            }
+        }
+
+        return secondMax;
     }
 
-    public static boolean isSorted(int [] array) {
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
         // Write your code here:
-        return false;
+        boolean contain = false;
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    contain = true;
+                    break;
+                } else {
+                    contain = false;
+
+                }
+            }
+
+        }
+
+        return contain;
+    }
+
+    public static boolean isSorted(int[] array) {
+        // Write your code here:
+        boolean sotred = false;
+        for (int i = 0; i < array.length; i++) {
+            int min = array[0];
+            if (array[i + 1] < min) {
+                return sotred;
+            } else {
+                sotred = true;
+                break;
+            }
+        }
+
+        return sotred;
     }
 
 }
+
